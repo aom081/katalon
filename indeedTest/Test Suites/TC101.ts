bcf024f9f556a9f83cff2a43c716c89fcac2e95c
nattavot014 64/45 <120540313+aom081@users.noreply.github.com>
2023-01-27 11:44:45 +0700

    Add files via upload

---
+++ indeedTest/Test Suites/TC101.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC101</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7af0b71b-1d7b-4e5a-b728-9f3e452f9491</testSuiteGuid>
+   <testCaseLink>
+      <guid>55f65467-2cc6-41ba-ba9f-10d2df318c32</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1003</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
